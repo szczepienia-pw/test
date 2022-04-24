cf4bf4c48ac2eaaec364b484e60d41a0d2b85b9c
Brotholomew <bartoszek.blach@gmail.com>
2022-04-24 21:37:20 +0200

    [SP-192][SP-193] created e2e test structure and admin e2e tests
    details in README.md :D
    
    Signed-off-by: Brotholomew <bartoszek.blach@gmail.com>

---
+++ szczepienia.test.py
@@ -0,0 +1,14 @@
+import unittest
+import selenium
+
+from utils import Mailbox
+from config import Config
+
+# tests
+from test_admin import AdminTests
+from test_user import UserTests
+from test_doctor import DoctorTests
+
+if __name__ == '__main__':
+    Config.set_up()
+    unittest.main(argv=Config.parse_test_args())
